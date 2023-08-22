07857d3ae0dd00a789a43f1354699addec36b949
Amrit Sahu <as8722@ic.ac.uk>
2023-08-22 18:24:07 +0100

    commented code

--- code/src/main/scala/HashFunction.scala
+++ code/src/main/scala/HashFunction.scala
@@ -4,26 +4,34 @@ import scala.io.Source
 import java.security.MessageDigest
 import java.nio.charset.StandardCharsets
 
-
+// Define a class named HashFunction
 class HashFunction:
+
+  // Define a method named generate that takes a plaintext string and a hash_type string as arguments
   def generate(plaintext: String, hash_type: String): String = {
+    // Create an instance of MessageDigest using the specified hash_type
     var hasher = MessageDigest.getInstance(hash_type)
+
+    // Calculate the hash (digest) of the plaintext by converting it to bytes and passing it to the MessageDigest
     val digested = hasher.digest(plaintext.getBytes())
+
+    // Create a StringBuilder to construct the hexadecimal representation of the digest
     val sb = new java.lang.StringBuilder()
+
+    // Iterate through each byte in the digest
     for (i <- 0 to digested.length - 1) {
+      // Convert the byte to a hexadecimal string (0xff is a bit mask to ensure we get a positive value)
       val hex = Integer.toHexString(0xff & digested(i))
+
+      // If the hexadecimal value is only one character, add a leading '0'
       if (hex.length == 1) {
         sb.append('0')
       }
+
+      // Append the hexadecimal value to the StringBuilder
       sb.append(hex)
     }
+
+    // Return the final hexadecimal representation of the hash
     return sb.toString()
   }
-
-
-
-
-
-
-
-
--- code/src/main/scala/PasswordStrengthTester.scala
+++ code/src/main/scala/PasswordStrengthTester.scala
@@ -1,23 +1,30 @@
 package password_strength_tester
 
+// Define a class named PasswordStrengthTester
 class PasswordStrengthTester:
 
+  // Define a method named test that takes a password string as input and returns a strength evaluation
   def test(password: String): String = {
-    var result = ""
-    var numcount = 0
-    var symbolcount = 0
-    val len = password.length()
-    val range = 0 to (len - 1)
+    var result = "" // Initialize a variable to store the strength evaluation
+    var numcount = 0 // Initialize a counter for the number of digits in the password
+    var symbolcount = 0 // Initialize a counter for the number of symbols in the password
+    val len = password.length() // Get the length of the password
+    val range = 0 to (len - 1) // Create a range from 0 to (length - 1) to iterate over the password characters
+
+    // Check if the length of the password is less than or equal to 8
     if (len <= 8)
       return "Weak. Increase the length of your password."
 
+    // Iterate through each character in the password
     for (i <- range)
+    // Check if the character is a digit (ASCII values between 48 and 57)
       if (password(i) > 47 && password(i) < 58)
-        numcount += 1
+        numcount += 1 // Increment the digit count
+      // Check if the character is a symbol (ASCII values outside the ranges of letters and digits)
       else if ((password(i) > 32 && password(i) < 48) || (password(i) > 57 && password(i) < 65) || (password(i) > 90 && password(i) < 97) || password(i) > 122)
-        symbolcount += 1
+        symbolcount += 1 // Increment the symbol count
 
-
+    // Evaluate the strength of the password based on digit and symbol counts
     if (numcount == 0 && symbolcount == 0)
       return "Weak. Add numbers and symbols."
     else if (numcount == 0)
@@ -27,5 +34,5 @@ class PasswordStrengthTester:
     else
       return "Strong."
 
-    result
+    result // This line is never reached as it's after the return statements
   }
